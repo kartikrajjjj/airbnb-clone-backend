e960e7840ae95911326c92ca445af09d0904a849
kartikrajjjj <kartikraj9756@gmail.com>
2026-07-03 01:38:41 +0530

    started styling using tailwind css (styled default home page)

--- routes/hostRouter.js
+++ routes/hostRouter.js
@@ -3,6 +3,8 @@ const path= require('path');
 const express = require('express');
 const hostRouter = express.Router();
 
+const rootDir = require("../utils/pathUtil");
+
 hostRouter.get("/home",(req,res,next)=>{
     res.sendFile(path.join(__dirname,"../","views","addhome.html"));
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const userRouter = express.Router();
 
+const rootDir = require("../utils/pathUtil");
+
 userRouter.get("/",(req,res,next)=>{
         res.sendFile(path.join(__dirname,"../","views","all.html"));
 });
