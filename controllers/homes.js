f5c0f9634a433f01d47568886ac30cb5ddb4198a
kartikrajjjj <kartikraj9756@gmail.com>
2026-07-11 01:56:29 +0530

    seperated views in host/ and user/ folders for clarity

--- controllers/homes.js
+++ controllers/homes.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const Home= require("../models/home");
 exports.getAddHome = (req,res,next)=>{
-    res.render("addhome.ejs");
+    res.render("host/addhome.ejs");
 }
 
 
@@ -9,10 +9,10 @@ exports.postAddHome = (req,res,next)=>{
     const {address,price,photo}=req.body;
     const home = new Home(address, price, photo);
     home.save();
-    res.render("homeadded.ejs");
+    res.render("host/homeadded.ejs");
 }
 exports.goHome = (req,res,next)=>{
     Home.fetchAll(registeredHomes=>{
-        res.render('all',{registeredHomes});
+        res.render('user/all',{registeredHomes});
     });
 };
